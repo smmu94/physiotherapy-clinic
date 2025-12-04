1e13ec2ddcf364f5ea2cacd2fe38b6cdcb749290
smmu94 <stephaniemmu@gmail.com>
2025-12-04 13:01:10 -0500

    Update font size in hero section in home

--- src/components/layout/footer/index.tsx
+++ src/components/layout/footer/index.tsx
@@ -3,26 +3,40 @@ import Link from "next/link";
 import { FaFacebook, FaInstagram, FaLinkedin } from "react-icons/fa";
 import { privacyItems } from "./constants";
 import { navItems } from "../navbar/constants";
+
 export default function Footer() {
   const t = useTranslations("common");
+
   return (
-    <footer className="flex flex-col gap-6 text-preset-4 py-6 border-t border-gray">
-      <div className="flex gap-16 justify-center items-center"> 
+    <footer className="flex flex-col gap-6 text-preset-4 py-6 border-t border-gray px-6 md:px-10">
+      <div className="flex flex-wrap justify-center gap-6 md:gap-16">
         {navItems.map((item) => (
-          <Link href={item.href} key={item.href} className="hover:text-primary transition-colors">{t(item.label)}</Link>
+          <Link
+            href={item.href}
+            key={item.href}
+            className="hover:text-primary transition-colors"
+          >
+            {t(item.label)}
+          </Link>
         ))}
       </div>
-      <div className="flex gap-6 justify-center items-center"> 
+      <div className="flex flex-wrap justify-center gap-6">
         {privacyItems.map((item) => (
-          <Link href={item.href} key={item.href} className="hover:text-primary transition-colors">{t(item.label)}</Link>
+          <Link
+            href={item.href}
+            key={item.href}
+            className="hover:text-primary transition-colors"
+          >
+            {t(item.label)}
+          </Link>
         ))}
       </div>
-      <div className="text-center flex gap-4 justify-center items-center">
+      <div className="flex justify-center gap-4">
         <FaFacebook className="cursor-pointer hover:text-primary transition-colors" size={20} />
         <FaInstagram className="cursor-pointer hover:text-primary transition-colors" size={20} />
         <FaLinkedin className="cursor-pointer hover:text-primary transition-colors" size={20} />
       </div>
-      <div className="text-center text-preset-5 ">
+      <div className="text-center text-preset-5">
         <p>&copy; {new Date().getFullYear()} ELON Physio, {t("footer.rights")}</p>
       </div>
     </footer>
